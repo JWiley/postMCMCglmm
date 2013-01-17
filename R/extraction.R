8b5b058c96495236220dd8f4edb3ff6a5172fe53
Joshua Wiley <jwiley.psych@gmail.com>
2013-01-17 00:54:16 -0800

    added documation to extraction functions and roxygenized

--- R/extraction.R
+++ R/extraction.R
@@ -1,5 +1,24 @@
-# extract fixed and random effect parameter names
-# from an MCMCglmm object
+#' Extract the parameter names from an \code{MCMCglmm} object
+#'
+#' Simple function to extract the fixed and random effects
+#' parameter names from an \code{MCMCglmm} object.
+#'
+#' @param object An \code{MCMCglmm} object
+#' @param \dots not used
+#' @return A list with two elements:
+#'   \item{fixed}{A character vector of the fixed effects parameter names}
+#'   \item{random}{A character vector of the random effects parameter names}
+#' @export
+#' @seealso \code{\link{fixef.MCMCglmm}} \code{\link{ranef.MCMCglmm}}
+#' @examples
+#' \dontrun{
+#'   # a simple MCMCglmm model
+#'   data(PlodiaPO)
+#'   m <- MCMCglmm(PO ~ 1, random = ~ FSfamily, data = PlodiaPO, verbose=FALSE)
+#'
+#'   # extract the parameter names
+#'   paramNamesMCMCglmm(m)
+#' }
 paramNamesMCMCglmm <- function(object, ...) {
   fNames <- as.character(attr(terms(object$Fixed$formula), "variables"))[-c(1, 2)]
 
@@ -16,8 +35,22 @@ paramNamesMCMCglmm <- function(object, ...) {
   list(fixed = fNames, random = rNames)
 }
 
-# extract the levels of the random effects
-# from an MCMCglmm object
+#' Extract the levels of factors used for random effects in \code{MCMCglmm} objects
+#'
+#' @param object An \code{MCMCglmm} model object
+#' @param data The dataset used for the model
+#' @param \dots Not currently used
+#' @export
+#' @seealso \code{\link{paramNamesMCMCglmm}} \code{\link{ranef.MCMCglmm}}
+#' @examples
+#' \dontrun{
+#'   # a simple MCMCglmm model
+#'   data(PlodiaPO)
+#'   m <- MCMCglmm(PO ~ 1, random = ~ FSfamily, data = PlodiaPO, verbose=FALSE)
+#'
+#'   # extract the random effects levels
+#'   ranefLevels(m, PlodiaPO)
+#' }
 ranefLevels <- function(object, data, ...) {
   n <- paramNamesMCMCglmm(object)$random
   res <- lapply(n, function(n) {
@@ -27,11 +60,23 @@ ranefLevels <- function(object, data, ...) {
   return(res)
 }
 
-# extract the fixed and random effects portions from an MCMCglmm object
-# note for the random, these are the estimates themselves
-# not the variability in the estimates
-# two use options let you get either just the posterior mean
-# or all the posterior samples
+#' Internal function to extract the fixed or random effects from an \code{MCMCglmm} object
+#'
+#' Extracts the fixed or random effects portions from an MCMCglmm object.
+#' Note for the random, these are the estimates themselves,
+#' not the variability in the estimates. The \code{use} options let you
+#' get either just the posterior mean or all the posterior samples.
+#'
+#' @param object An \code{MCMCglmm} object
+#' @param use A character string indicating whether to return \dQuote{all} the
+#'   posterior samples (the default) or only the \dQuote{mean} of them.
+#' @param which A character string indicating whether to return the
+#'   \dQuote{fixed} (the default) or \dQuote{random} effects.
+#' @param \dots Not currently used.
+#' @return A matrix of the posterior samples or means for the fixed or random effects.
+#' @keywords internal
+#' @seealso \code{\link{fixef.MCMCglmm}} \code{\link{ranef.MCMCglmm}}
+#' @rdname extractEffects
 .extractEffects <- function(object, use = c("all", "mean"),
   which = c("fixed", "random"), ...) {
 
@@ -65,26 +110,21 @@ ranefLevels <- function(object, data, ...) {
     mean = as.matrix(colMeans(b)))
 }
 
-
-# import generics
-# (cheap version of importFrom() in a package)
-fixef <- nlme:::fixef
-ranef <- nlme:::ranef
-
-
-#' Extract fixed effects from an MCMCglmm object
+#' Extract fixed effects from an \code{MCMCglmm} object
 #'
 #' Function designed to extract the fixed effects from an
-#' MCMCglmm model object. Can either extract all samples from the
+#' \code{MCMCglmm} model object. Can either extract all samples from the
 #' fixed effects posteriors or return the posterior means.
 #'
-#' @param object An MCMCglmm model object to extract the effects from
+#' @param object An \code{MCMCglmm} model object to extract the effects from
 #' @param use A character string indicating whether to extract
 #'   all posterior samples or the mean of the posteriors. Defaults to
 #'   "all".
 #' @param \dots Arguments passed on to the worker function.
 #' @return A matrix of the fixed effects
+#' @importFrom nlme fixef
 #' @S3method fixef MCMCglmm
+#' @seealso \code{\link{ranef.MCMCglmm}}
 #' @examples
 #' \dontrun{
 #'   # a simple MCMCglmm model
@@ -103,19 +143,21 @@ fixef.MCMCglmm <- function(object, use = c("all", "mean"), ...) {
   .extractEffects(object = object, use = use, which = "fixed", ...)
 }
 
-#' Extract random effects from an MCMCglmm object
+#' Extract random effects from an \code{MCMCglmm} object
 #'
 #' Function designed to extract the random effects from an
-#' MCMCglmm model object. Can either extract all samples from the
+#' \code{MCMCglmm} model object. Can either extract all samples from the
 #' random effects posteriors or return the posterior means.
 #'
-#' @param object An MCMCglmm model object to extract the effects from
+#' @param object An \code{MCMCglmm} model object to extract the effects from
 #' @param use A character string indicating whether to extract
 #'   all posterior samples or the mean of the posteriors. Defaults to
 #'   "all".
 #' @param \dots Arguments passed on to the worker function.
 #' @return A matrix of the fixed effects
+#' @importFrom nlme ranef
 #' @S3method ranef MCMCglmm
+#' @seealso \code{\link{fixef.MCMCglmm}}
 #' @examples
 #' \dontrun{
 #'   # a simple MCMCglmm model
